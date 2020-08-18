1828e1703378b4e7fb3d7fd05b0bd35476ee6471
dzendzen <dzen@hotmail.fr>
2020-08-18 16:17:21 +0200

    fetch cpntact en cours

--- back/server.js
+++ back/server.js
@@ -5,18 +5,13 @@ const api = require("./routes");
 const cors = require("cors");
 
 require("dotenv").config();
-const SERVER_ADDRESS = process.env.SERVER_ADDRESS;
+const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS;
 
 app.use(parser.json());
-// app.use(parser.urlencoded({ extended: true }));
+app.use(parser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api", api);
-
-// home page
-// app.get("/", (req, res) => {
-//   res.send("hello world");
-// });
 
 const server = app.listen(8000, () => {
   console.log("server is listening on port 8000");
